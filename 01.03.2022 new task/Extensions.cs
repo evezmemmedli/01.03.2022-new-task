5cb5c63ab1f0178be179b648c5a7925ed7afec21
evezmemmedli <avazkm@code.edu.az>
2022-02-28 15:51:41 +0400

    Add project files.

---
+++ 01.03.2022 new task/Extensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._03._2022_new_task
+{
+    static class Extensions
+    {
+        public static string isCapitalized(this string word)
+        { 
+            //Task1
+            #region
+            string capitaledWord = "";
+            word = word.ToLower();
+            var newWord = word.Split(' ');
+            foreach (string item in newWord)
+            {
+                capitaledWord += char.ToUpper(item[0]); /*+item.Substring(1) + " ";*/
+                for (int i = 1; i < item.Length; i++)
+                {
+                    capitaledWord += item[i];
+
+                }
+                capitaledWord += " ";
+            }
+            return capitaledWord;
+
+            #endregion
+
+            
+        }
+
+        //Task2
+        #region
+
+        public static bool SearchWord(this string text, string search)
+
+        {
+            string[] newtext = text.Split(" ");
+            foreach (string item in newtext)
+            {
+                if (item == search)
+                {
+                    return true;
+                }
+            }
+            return false;
+            #endregion
+
+        }
+    }
+}
---
+++ 01.03.2022 new task/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01._03._2022_new_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "exTensiON claSSi staTIc OLMalIdiR";
+            Console.WriteLine(word.isCapitalized());
+
+            string text = "Salam usaqlar necesiniz ?";
+            Console.WriteLine(text.SearchWord("ADFDS"));
+        }
+    }
+}
